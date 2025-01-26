ae0da73520086fadeaea735822e088d209e23782
Burton Smith <burton@breakstuff.io>
2025-01-26 10:51:01 -0500

    add deep merge

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,6 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
+  tseslint.configs.recommended,
   eslintConfigPrettier,
 );
