0be31ee60e6380a2b8a106b355045f7173de3e04
Burton Smith <burton@breakstuff.io>
2025-01-28 16:24:34 -0500

    add utils

---
+++ src/utils/cem-utils.test.ts
@@ -0,0 +1,199 @@
+import { describe, expect, test } from "vitest";
+import {
+  getAllComponents,
+  getComponentByClassName,
+  getComponentByTagName,
+  getComponentEventsWithType,
+  getComponentPublicMethods,
+  getComponentPublicProperties,
+  getCustomEventDetailTypes,
+} from "./cem-utils";
+import { shoelaceCem } from "./__MOCKS__/shoelace-cem";
+import { guiCem } from "./__MOCKS__/gui-cem";
+
+describe("getAlComponents", () => {
+  test("should return one component from `guiCem`", () => {
+    // Arrange
+
+    // Act
+    const components = getAllComponents(guiCem);
+
+    // Assert
+    expect(components.length).toEqual(1);
+  });
+
+  test("should return an array of all components from `shoelaceCem`", () => {
+    // Arrange
+
+    // Act
+    const components = getAllComponents(shoelaceCem);
+
+    // Assert
+    expect(components.length).toEqual(58);
+  });
+});
+
+describe("getComponentByClassName", () => {
+  test("should return icon component from `guiCem`", () => {
+    // Arrange
+
+    // Act
+    const component = getComponentByClassName(guiCem, "GuiIcon");
+
+    // Assert
+    expect(component).toBeDefined();
+  });
+
+  test("should return `undefined` when component is not found", () => {
+    // Arrange
+
+    // Act
+    const component = getComponentByClassName(guiCem, "GuiIcon2");
+
+    // Assert
+    expect(component).toBeUndefined();
+  });
+
+  test("should return icon component from `shoelaceCem`", () => {
+    // Arrange
+
+    // Act
+    const component = getComponentByClassName(shoelaceCem, "SlIcon");
+
+    // Assert
+    expect(component).toBeDefined();
+  });
+
+  test("should return `undefined` when component is not found", () => {
+    // Arrange
+
+    // Act
+    const component = getComponentByClassName(shoelaceCem, "SlIcon2");
+
+    // Assert
+    expect(component).toBeUndefined();
+  });
+});
+
+describe("getComponentByTagName", () => {
+  test("should return icon component from `guiCem`", () => {
+    // Arrange
+
+    // Act
+    const component = getComponentByTagName(guiCem, "gui-icon");
+
+    // Assert
+    expect(component).toBeDefined();
+  });
+
+  test("should return `undefined` when component is not found", () => {
+    // Arrange
+
+    // Act
+    const component = getComponentByTagName(guiCem, "gui-icon2");
+
+    // Assert
+    expect(component).toBeUndefined();
+  });
+
+  test("should return icon component from `shoelaceCem`", () => {
+    // Arrange
+
+    // Act
+    const component = getComponentByTagName(shoelaceCem, "sl-icon");
+
+    // Assert
+    expect(component).toBeDefined();
+  });
+
+  test("should return `undefined` when component is not found", () => {
+    // Arrange
+
+    // Act
+    const component = getComponentByTagName(shoelaceCem, "sl-icon2");
+
+    // Assert
+    expect(component).toBeUndefined();
+  });
+});
+
+describe("getComponentPublicProperties", () => {
+  test("should return 2 public properties for `GuiIcon`", () => {
+    // Arrange
+    const component = getComponentByClassName(guiCem, "GuiIcon");
+
+    // Act
+    const props = getComponentPublicProperties(component!);
+
+    // Assert
+    expect(props.length).toEqual(2);
+  });
+});
+
+describe("getPublicMethods", () => {
+  test("should return 2 public methods for `SlAlert`", () => {
+    // Arrange
+    const component = getComponentByClassName(shoelaceCem, "SlAlert");
+
+    // Act
+    const methods = getComponentPublicMethods(component!);
+
+    // Assert
+    expect(methods.length).toEqual(5);
+  });
+});
+
+describe("getComponentEventsWithType", () => {
+  test("should return 4 events for `SlAlert`", () => {
+    // Arrange
+    const component = getComponentByClassName(shoelaceCem, "SlAlert");
+
+    // Act
+    const events = getComponentEventsWithType(component!);
+
+    // Assert
+    expect(events.length).toEqual(4);
+    expect(events[0].type.text).toEqual("CustomEvent<SlShowType>");
+    expect(events[1].type.text).toEqual("CustomEvent");
+  });
+
+  test("should override event names for `SlAlert`", () => {
+    // Arrange
+    const component = getComponentByClassName(shoelaceCem, "SlAlert");
+
+    // Act
+    const events = getComponentEventsWithType(component!, {
+      overrideCustomEventType: true,
+    });
+
+    // Assert
+    expect(events[0].type.text).toEqual("SlShowType");
+    expect(events[1].type.text).toEqual("CustomEvent");
+  });
+
+  test("should override event type for `SlAlert`", () => {
+    // Arrange
+    const component = getComponentByClassName(shoelaceCem, "SlAlert");
+
+    // Act
+    const events = getComponentEventsWithType(component!, {
+      customEventDetailTypePropName: "parsedType",
+    });
+
+    // Assert
+    expect(events[1].type.text).toEqual("CustomEvent<'value 1' | 'value 2' | 'value 3' | 'value 4'>");
+  });
+});
+
+describe("getCustomEventDetailTypes", () => {
+  test("should return 2 custom event detail types for `SlAlert`", () => {
+    // Arrange
+    const component = getComponentByClassName(shoelaceCem, "SlAlert");
+
+    // Act
+    const eventTypes = getCustomEventDetailTypes(component!);
+    
+    // Assert
+    expect(eventTypes?.length).toEqual(2);
+  });
+});
---
+++ src/utils/cem-utils.ts
@@ -0,0 +1,171 @@
+import type * as cem from "custom-elements-manifest";
+
+export const EXCLUDED_TYPES = [
+  "any",
+  "bigint",
+  "boolean",
+  "never",
+  "null",
+  "number",
+  "string",
+  "Symbol",
+  "undefined",
+  "unknown",
+];
+
+/**
+ * Gets a list of all components from a Custom Elements Manifest object
+ * @param customElementsManifest
+ * @param exclude an array of component names to exclude
+ * @returns CustomElement[]
+ */
+export function getAllComponents(
+  customElementsManifest: unknown,
+  exclude: string[] = []
+) {
+  return (
+    (customElementsManifest as cem.Package).modules
+      ?.map((mod) =>
+        mod.declarations
+          ?.filter((d) => (d as cem.CustomElement).customElement)
+          ?.flat()
+      )
+      ?.flat() || []
+  ).filter((x) => x && !exclude?.includes(x.name)) as cem.CustomElement[];
+}
+
+/**
+ * Gets a component from a CEM object based on the class name
+ * @param customElementsManifest CEM object
+ * @param exclude and array of component names to exclude
+ * @returns Component
+ */
+export function getComponentByClassName(
+  customElementsManifest: unknown,
+  className?: string
+) {
+  return getAllComponents(customElementsManifest).find(
+    (c) => c.name === className
+  );
+}
+
+/**
+ * Gets a component from a CEM object based on the tag name
+ * @param customElementsManifest CEM object
+ * @param exclude and array of component names to exclude
+ * @returns Component
+ */
+export function getComponentByTagName(
+  customElementsManifest: unknown,
+  tagName?: string
+) {
+  return getAllComponents(customElementsManifest).find(
+    (c) => c.tagName === tagName
+  );
+}
+
+/**
+ * Gets a list of public properties from a CEM component
+ * @param component CEM component/declaration object
+ * @returns an array of public properties for a given component
+ */
+export function getComponentPublicProperties(component: cem.CustomElement) {
+  return (component?.members?.filter(
+    (member) =>
+      member.kind === "field" &&
+      member.privacy !== "private" &&
+      member.privacy !== "protected" &&
+      !member.static &&
+      !member.name.startsWith("#")
+  ) || []) as cem.ClassMember[];
+}
+
+/**
+ * Get all public methods for a component
+ * @param component CEM component/declaration object
+ * @returns ClassMethod[]
+ */
+export function getComponentPublicMethods(
+  component: cem.CustomElement
+): cem.ClassMethod[] {
+  return (component?.members?.filter(
+    (member) =>
+      member.kind === "method" &&
+      member.privacy !== "private" &&
+      member.privacy !== "protected" &&
+      !member.name.startsWith("#")
+  ) || []) as cem.ClassMethod[];
+}
+
+export type EventOptions = {
+  /** The name of the property where custom detail type is stored */
+  customEventDetailTypePropName?: string;
+  /** Overrides the event type from `CustomEvent` to the type specified in the event type in the CEM */
+  overrideCustomEventType?: boolean;
+};
+
+/**
+ * Get all events for a component with the complete event type
+ * @param component CEM component/declaration object
+ * @param options {EventOptions} options for custom event detail type and custom event type
+ * @returns Event[]
+ */
+export function getComponentEventsWithType(
+  component: cem.CustomElement,
+  options: EventOptions = {}
+) {
+  const events = component?.events?.map((e) => {
+    const type: string =
+      (e as unknown as Record<string, cem.Type>)[
+        `${options.customEventDetailTypePropName}`
+      ]?.text || e.type?.text;
+
+    const eventType = options.overrideCustomEventType
+      ? type || "CustomEvent"
+      : type
+        ? `CustomEvent<${type}>`
+        : "CustomEvent";
+
+    return {
+      ...e,
+      type: {
+        text: eventType,
+      },
+    };
+  });
+
+  return events || [];
+}
+
+/**
+ * Gets a list of event names for a given component.
+ * This is used for generating a list of event names for an import in a type definition file.
+ * If the event detail type is not a named type, custom type, or a generic, it will not be included in the list.
+ * @param component The component you want to get the event types for
+ * @param excludedTypes Any types you want to exclude from the list
+ * @returns A string array of event types for a given component
+ */
+export function getCustomEventDetailTypes(
+  component: cem.CustomElement,
+  excludedTypes?: string[]
+) {
+  const types =
+    component?.events
+      ?.map((e) => {
+        const eventType = e.type?.text
+          .replace("[]", "")
+          .replace(" | undefined", "");
+        return eventType &&
+          !excludedTypes?.includes(eventType) &&
+          !EXCLUDED_TYPES.includes(eventType) &&
+          !eventType.includes("<") &&
+          !eventType.includes(`{`) &&
+          !eventType.includes("'") &&
+          !eventType.includes(`"`)
+          ? eventType
+          : undefined;
+      })
+      ?.filter((e) => e !== undefined && !e?.startsWith("HTML")) || [];
+
+  return types?.length ? [...new Set(types)] : undefined;
+}
