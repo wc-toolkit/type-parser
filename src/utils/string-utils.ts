01413e91df5cf28d96f03c9cb5bb16135a8c5f56
Burton Smith <burton@breakstuff.io>
2025-02-09 13:09:42 -0500

    update function name

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,14 +1,18 @@
+/**
+ * Convert a string to kebab-case 
+ * (ex: "my-component" from "MyComponent")
+ * @param value 
+ * @returns {string}
+ */
 export const toKebabCase = (value: string): string =>
   value.replace(/([a-z0–9])([A-Z])/g, "$1-$2").toLowerCase();
 
-export function removeQuoteWrappers(value: string) {
-  return value.trim().replace(/^["'](.+(?=["']$))["']$/, "$1");
-}
-
-export function has(arr?: unknown[]) {
-  return Array.isArray(arr) && arr.length > 0;
-}
-
+/**
+ * Convert a string to sentence case
+ * (ex: "My component" from "myComponent")
+ * @param value
+ * @returns {string}
+ */
 export function toSentenceCase(value: string) {
   return (
     value
@@ -24,6 +28,12 @@ export function toSentenceCase(value: string) {
   );
 }
 
+/**
+ * Convert a string to pascal case
+ * (ex: "MyComponent" from "my-component")
+ * @param value
+ * @returns {string}
+ */
 export function toPascalCase(value: string) {
   return value
     .replace(new RegExp(/[-_]+/, "g"), " ")
@@ -35,6 +45,12 @@ export function toPascalCase(value: string) {
     .replace(new RegExp(/\w/), (s) => s.toUpperCase());
 }
 
+/**
+ * Convert a string to camel case
+ * (ex: "myComponent" from "my-component")
+ * @param value
+ * @returns {string}
+ */
 export function toCamelCase(value: string = "") {
   const arr = value.split("-");
   const capital = arr.map((item, index) =>
@@ -43,4 +59,14 @@ export function toCamelCase(value: string = "") {
       : item.toLowerCase(),
   );
   return capital.join("");
+}
+
+/**
+ * Removes quote wrappers from a string (single or double quotes) 
+ * (ex: "my-component" from "'my-component'")
+ * @param value
+ * @returns {string}
+ */
+export function removeQuotes(value: string) {
+  return value.trim().replace(/^["'](.+(?=["']$))["']$/, "$1");
 }
